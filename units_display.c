41c4d35c810e68fb6705076e0f08f6ef27afd547
Jamesmakumi <makumichege57@gmail.com>
2025-09-25 20:15:47 +0300

    Add files via upload

---
+++ units_display.c
@@ -0,0 +1,31 @@
+/*
+Name:James chege Makumi
+Reg:CT101/G/26431/25
+Description:units display
+*/ 
+
+#include<stdio.h>
+
+//main function 
+
+int main(){
+	//Display of units
+	printf("SPC2101 Introduction to programming and problem solving");
+	
+	printf("\n UCU2101 Communication skills");
+	
+	printf("\n SPC2102 Introduction to computer architecture");
+	
+	printf("\n UCU2105 Fundamentals of ICT");
+	
+	printf("\n SPH2107 Analogue electronics");
+	
+	printf("\n SPM2104 Mathematics for science");
+	
+	printf("\n SPH2101 Physics");
+	
+	printf("\n SPM2100 Discrete mathematics");
+	
+	return 0;
+	
+}
